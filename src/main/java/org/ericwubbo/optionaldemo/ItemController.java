f96ecb875fbebe7c2d3f44c7ecc33d4fd52ad5e8
Eric-Wubbo Lameijer <eric_wubbo@hotmail.com>
2024-05-12 14:22:13 +0200

    Using Optional

--- src/main/java/org/ericwubbo/optionaldemo/ItemController.java
+++ src/main/java/org/ericwubbo/optionaldemo/ItemController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequiredArgsConstructor
 public class ItemController {
@@ -17,7 +19,9 @@ public class ItemController {
 
     @GetMapping("{name}")
     public Item getByName(@PathVariable String name) {
-        Item item = itemRepository.findByName(name);
+        Optional<Item> possibleItem = itemRepository.findByName(name);
+        if (possibleItem.isEmpty()) return null;
+        Item item = possibleItem.get();
         String storedName = item.getName();
         String moreImpressiveName = Character.toUpperCase(storedName.charAt(0)) + storedName.substring(1);
         item.setName(moreImpressiveName); 
--- src/main/java/org/ericwubbo/optionaldemo/ItemRepository.java
+++ src/main/java/org/ericwubbo/optionaldemo/ItemRepository.java
@@ -3,6 +3,8 @@ package org.ericwubbo.optionaldemo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
-    Item findByName(String name);
+    Optional<Item> findByName(String name);
 }
